6055c1a17eaaf7e0d2f6c0dc3d8b04ab715feaab
Elou-Zamkol <raulabdurahmanli2006@gmail.com>
2025-04-17 21:34:02 +0400

    Add files via upload

---
+++ Network-DZ-1/DZlin1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+var address = IPAddress.Parse("172.20.28.184");
+var serverEndPoint = new IPEndPoint(address, 3003);
+
+try
+{
+    clientSocket.Connect(serverEndPoint);
+    
+    while (true)
+    {
+        Console.WriteLine("Enter message to send to server or type 'quit' to exit:");
+        string message = Console.ReadLine();
+
+        byte[] messageBytes = System.Text.Encoding.UTF8.GetBytes(message);
+        clientSocket.Send(messageBytes);
+        Console.WriteLine($"Sent message: {message}");
+        
+        byte[] Buffer = new byte[1024];
+        int BytesRead = clientSocket.Receive(Buffer);
+        string response = System.Text.Encoding.UTF8.GetString(Buffer, 0, BytesRead);
+        Console.WriteLine($"Server responded: {response}");
+
+        if (message.ToLower() == "quit")
+        {
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+            break;
+        }
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
---
+++ Network-DZ-1/Server/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+var address = IPAddress.Parse("172.20.28.184");
+
+var endPoint = new IPEndPoint(address, 3003);
+
+
+
+try
+{
+    serverSocket.Bind(endPoint);
+    serverSocket.Listen();
+    Console.WriteLine($"Listening on {endPoint.Address}:{endPoint.Port}");
+    while (true)
+    {
+        var buffer = new byte[1024];
+        var clientSocket = serverSocket.Accept();
+        Console.WriteLine($"Client connected: {clientSocket.RemoteEndPoint}");
+        
+        ThreadPool.QueueUserWorkItem(state =>
+        {
+            
+            while (true)
+            {
+                var bytesRead = clientSocket.Receive(buffer);
+                var message = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Received message: {message}");
+        
+                string ResponseMessage = $"Server received: {message}";
+                var ResponseBytes = System.Text.Encoding.UTF8.GetBytes(ResponseMessage);
+                clientSocket.Send(ResponseBytes);
+        
+       
+                if (message.ToLower() == "quit")
+                {
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                    break;
+                }
+            }
+            
+        }, clientSocket);
+        
+        
+    }
+    
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
