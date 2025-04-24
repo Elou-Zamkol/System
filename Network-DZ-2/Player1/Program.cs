ce2b8971c73c9312661435c7dc70d4c52eadbc5c
Elou-Zamkol <raulabdurahmanli2006@gmail.com>
2025-04-24 11:16:29 +0400

    Add files via upload

---
+++ Network-DZ-2/Player1/Program.cs
@@ -0,0 +1,199 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+
+
+static bool SiteCheck(int highlightX, int highlightY, string masiv)
+{
+    for (int y = 0; y < 3; y++)
+    {
+        for (int x = 0; x < 3; x++)
+        {
+            if (masiv[x + (y * 3)] != ' ' && x == highlightX && y == highlightY)
+            {
+                return false;
+                
+            }
+        }
+    }
+    return true;
+}
+
+
+
+static void DrawField(int highlightX, int highlightY, string masiv)
+{
+    for (int y = 0; y < 3; y++)
+    {
+        for (int x = 0; x < 3; x++)
+        {
+            if (masiv[x + (y * 3)] != ' ')
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write($"[{masiv[x + (y * 3)]}]");
+                Console.ResetColor();
+            }
+            
+            else if (x == highlightX && y == highlightY)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("[ ]");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.Write("[ ]");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+static void EndWin(string masiv, string end)
+{
+    int count = 0;
+    for (int y = 0; y < 3; y++)
+    {
+        for (int x = 0; x < 3; x++)
+        {
+
+            if (Convert.ToInt32(Convert.ToString(end[0 + count])) == y && Convert.ToInt32(Convert.ToString(end[1 + count])) == x)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write($"[{masiv[x + (y * 3)]}]");
+                Console.ResetColor();
+                
+                if(count < 4) count += 2;
+                
+            }
+             
+            else if (masiv[x + (y * 3)] != ' ')
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write($"[{masiv[x + (y * 3)]}]");
+                Console.ResetColor();
+            }
+            
+            else
+            {
+                Console.Write("[ ]");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+
+static async Task<string> Expectation(StreamReader reader)
+{
+    Console.Write("Wait for player's turn!!!");
+    string? masiv = await reader.ReadLineAsync();
+    Console.Clear();
+    return masiv;
+}
+
+//---------------------------------------------------------------
+
+
+
+
+using var client = new TcpClient();
+await client.ConnectAsync("172.17.0.1", 3003);
+
+using var networkStream = client.GetStream();
+using var writer = new StreamWriter(networkStream, Encoding.UTF8) { AutoFlush = true };
+using var reader = new StreamReader(networkStream, Encoding.UTF8);
+
+int cursorX = 0;
+int cursorY = 0;
+bool FlagEnd = true;
+
+while (true)
+{
+    bool flag = true;
+    
+    Console.CursorVisible = false;
+    string masiv = await Expectation(reader);
+    Console.CursorVisible = true;
+    
+    string ?end = await reader.ReadLineAsync();
+    
+
+    if (end.Length == 6)
+    {
+        Console.WriteLine($"m- {masiv}");
+        Console.WriteLine($"m- {masiv.Length}");
+        char Simbol = masiv[Convert.ToInt32(Convert.ToString(end[0])) + (Convert.ToInt32(Convert.ToString(end[1])) * 3)];
+        
+        if (end == "Draw")
+        {
+            Console.Clear();
+            Console.WriteLine("---------Draw-----------");
+            FlagEnd = false;
+        }
+    
+        else if (Simbol == 'X')
+        {
+            Console.Clear();
+            EndWin(masiv, end);
+            Console.WriteLine("---------You Win-----------");
+            FlagEnd = false;
+        }
+    
+        else if (Simbol == 'O')
+        {
+            Console.Clear();
+            Console.WriteLine("---------You lost-----------");
+            FlagEnd = false;
+        }
+    }
+    
+    
+    
+    
+    Console.CursorVisible = false;
+
+    while (flag && FlagEnd)
+    {
+        Console.Clear();
+        DrawField(cursorX, cursorY, masiv);
+
+        ConsoleKeyInfo key = Console.ReadKey(true);
+
+        switch (key.Key)
+        {
+            case ConsoleKey.LeftArrow:
+                if (cursorX > 0) cursorX--;
+                break;
+            case ConsoleKey.RightArrow:
+                if (cursorX < 3 - 1) cursorX++;
+                break;
+            case ConsoleKey.UpArrow:
+                if (cursorY > 0) cursorY--;
+                break;
+            case ConsoleKey.DownArrow:
+                if (cursorY < 3 - 1) cursorY++;
+                break;
+            case ConsoleKey.Enter:
+                
+                if (SiteCheck(cursorX, cursorY, masiv))
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Вы выбрали ячейку: ({cursorX}, {cursorY})");
+                    flag = false;
+                }
+                
+                break;
+        }
+    }
+    
+    string message = string.Join(' ', cursorX, cursorY);
+    await writer.WriteLineAsync(message);
+    await writer.WriteLineAsync("X");
+    
+    if (!FlagEnd) break;
+    
+}
+
+
---
+++ Network-DZ-2/Server/Program.cs
@@ -0,0 +1,119 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+static string CheckWinnerOrGameStatus(string[,] game)
+{
+
+    for (int i = 0; i < 3; i++)
+    {
+        
+        if (game[i, 0] == game[i, 1] && game[i, 0] == game[i, 2] && game[i, 0] != null)
+        {
+            return $"{i}0{i}1{i}2";
+        }
+        
+        if (game[0, i] == game[1, i] && game[0, i] == game[2, i] && game[0, i] != null)
+        {
+            return $"0{i}1{i}2{i}";
+        }
+        
+        if (game[0, 0] == game[1, 1] && game[0, 0] == game[2, 2] && game[0, 0] != null)
+        {
+            return $"001122";
+        }
+    
+        if (game[0, 2] == game[1, 1] && game[0, 2] == game[2, 0] && game[0, 2] != null)
+        {
+            return $"021120";
+        }
+    }
+    
+    bool BoardFull = true;
+    for (int y = 0; y < 3; y++)
+    {
+        for (int x = 0; x < 3; x++)
+        {
+            if (game[y, x] == null || game[y, x] == " ")
+            {
+                BoardFull = false;
+                break;
+            }
+        }
+    }
+
+    if (BoardFull)
+    {
+        return "Draw";
+    }
+    
+    return "";
+    
+
+}
+
+TcpListener listener = new(IPAddress.Any, 3003);
+listener.Start();
+Console.WriteLine("Server started. Waiting for connections...");
+
+TcpClient client1 = await listener.AcceptTcpClientAsync();
+Console.WriteLine("Client 1 connected!");
+
+TcpClient client2 = await listener.AcceptTcpClientAsync();
+Console.WriteLine("Client 2 connected!");
+
+ManualResetEvent flag1 = new(false);
+ManualResetEvent flag2 = new(false);
+
+string[,] game = new string[3, 3];
+string end = "";
+
+Task RunClient(TcpClient client, ManualResetEvent myTurn, ManualResetEvent otherTurn)
+{
+    return Task.Run(async () =>
+    {
+        using NetworkStream stream = client.GetStream();
+        using var reader = new StreamReader(stream, Encoding.UTF8);
+        using var writer = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true };
+
+        while (true)
+        {
+            myTurn.WaitOne();
+
+            string masiv = "";
+
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    masiv += game[x, y] ?? " ";
+                }
+            }
+            
+            await writer.WriteLineAsync(masiv);
+            
+            end = CheckWinnerOrGameStatus(game);
+            
+            await writer.WriteLineAsync(end);
+
+            string? message = await reader.ReadLineAsync();
+            string? symbol = await reader.ReadLineAsync();
+
+            game[Convert.ToInt32(Convert.ToString(message[2])), Convert.ToInt32(Convert.ToString(message[0]))] = symbol;
+            
+            myTurn.Reset();
+            otherTurn.Set();
+            
+            if(end.Length == 6) break;
+        }
+    });
+}
+
+Task task1 = RunClient(client1, flag1, flag2);
+Task task2 = RunClient(client2, flag2, flag1);
+
+flag1.Set();
+
+await Task.WhenAll(task1, task2);
+
+
